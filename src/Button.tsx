35e12d25966ef71798c2110609ba3f33e9d47dc1
Jocelyn Proctor <jocelyn.proctor@sap.com>
2021-11-19 18:30:58 +0100

    created button, footer changed favicon

---
+++ src/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.css';
+
+function Button() {
+    return (
+        <button disabled className="Button">Get Started</button>
+    );
+}
+
+export default Button;
